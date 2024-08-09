69335638b6443011461a4482597a7bb07a8b27f7
Misbah Ul Hoq <extraordinarymisbah@gmail.com>
2024-08-09 12:10:24 +0600

    updated fetch.js

--- src/axios/fetch.js
+++ src/axios/fetch.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "http://localhost:3000",
   baseURL: "https://volunteer-management-system-api.vercel.app",
+  withCredentials: false,
 });
 
 export default api;
